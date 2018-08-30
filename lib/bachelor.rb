2f5b22ce980ce6f4bef219cad2fe76d8e131fc23
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-30 13:11:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,7 @@ def get_first_name_of_season_winner(data, season)
     if season_info == season
     all_contestant_info.each do |contestant|
       if contestant["status"] == "Winner"
-
+          p constant
       end
     end 
   end 
